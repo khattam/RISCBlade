66e76c9f78c5c6757d329d86531acd434c93dce1
Bennett <bennetev@rose-hulman.edu>
2024-01-28 23:10:33 -0500

    restored memory to more usable state. still incomplete

--- implementation/MEMORY.v
+++ implementation/MEMORY.v
@@ -1,13 +1,14 @@
 
 module MEMORY (
-input [15:0] MEM_ADDRESS, input [15:0] MEM_DATA, input MEMWRITE,
+input [15:0] MEM_ADDRESS, 
+input [15:0] MEM_DATA, 
+input MEMWRITE,
 output reg [15:0] MEM_OUT
 );
 
-reg [15:0] mem [0:65536];
-
+reg [15:0] mem [0:65535];
 	initial begin
-		if(MEMWRITE == 1) begin
+		if (MEMWRITE) begin
 			mem[MEM_ADDRESS] <= MEM_DATA;
 		end
 		MEM_OUT <= mem[MEM_ADDRESS];
