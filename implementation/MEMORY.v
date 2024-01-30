fe596703e92d54ea1401d60240966a82e901adc1
Bennett <bennetev@rose-hulman.edu>
2024-01-30 17:34:36 -0500

    WORKING MEMORY

--- implementation/MEMORY.v
+++ implementation/MEMORY.v
@@ -8,16 +8,16 @@ input RST,
 output reg [15:0] MEM_OUT
 );
 
-reg [15:0] mem [0:65535];
+reg [15:0] mem [0:499];
 integer i;
 	always @ (posedge CLK) begin
 		if(!RST) begin
-			for(i = 0; i < 65536; i = i + 1) begin
+			for(i = 0; i < 500; i = i + 1) begin
 				mem[i] = 0;
 			end
 		end
 		if (MEMWRITE) begin
-			mem[MEM_ADDRESS] <= MEM_DATA;
+			mem[MEM_ADDRESS] = MEM_DATA;
 		end
 		MEM_OUT <= mem[MEM_ADDRESS];
 	end
