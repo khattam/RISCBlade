0a442f40a10efc06efb1445f4ed868c79a2df5e1
rhit-carpenhm <carpenhm@rose-hulman.edu>
2024-02-04 19:56:07 -0500

    Fixed mux and test

--- implementation/MUX_TEST.v
+++ implementation/MUX_TEST.v
@@ -1,12 +1,14 @@
 `timescale 1ns/1ps
 
 module MUX_TEST;
-	reg [15:0] A_TEST
-	reg [15:0] B_TEST
-	reg select_TEST
-	wire [15:0] OUT_TEST
-	MUX uut(A_TEST, B_TEST, select_TEST, OUT_TEST);
+	reg [15:0] A_TEST;
+	reg [15:0] B_TEST;
+	reg select_TEST;
+	wire [15:0] MUXOUT_TEST;
+	MUX uut(A_TEST, B_TEST, select_TEST, MUXOUT_TEST);
 	
 	initial begin
-	A_TEST = 11111; B_TEST = 00000; select_test = 0; #10;
-	A_TEST = 11111; B_TEST = 00000; select_test = 1; #10;
+	A_TEST = 11111; B_TEST = 00000; select_TEST = 0; #10;
+	A_TEST = 11111; B_TEST = 00000; select_TEST = 1; #10;
+	end
+endmodule
