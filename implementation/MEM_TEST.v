7f8652d98024af1a75b82539f6f98f81ec8328f9
Bennett <bennetev@rose-hulman.edu>
2024-02-04 21:50:09 -0500

    working pc+alu integration

--- implementation/MEM_TEST.v
+++ implementation/MEM_TEST.v
@@ -5,24 +5,20 @@ module MEM_TEST;
 	reg [15:0] DATA_TEST;
 	reg WRITE_TEST;
 	reg CLK_TEST;
-	reg RST_TEST;
 	wire [15:0] MEMOUT_TEST;
 
-	MEMORY uut(ADDR_TEST, DATA_TEST, WRITE_TEST, CLK_TEST, RST_TEST, MEMOUT_TEST);
+	MEMORY uut(ADDR_TEST, DATA_TEST, WRITE_TEST, CLK_TEST, MEMOUT_TEST);
 	
 	//clock setup
 	initial begin
 		CLK_TEST = 0;
-		forever begin
-			#10 CLK_TEST = ~CLK_TEST;
-		end
 	end
+	
+	always #10 CLK_TEST = ~CLK_TEST;
 	
 	//tests
 	initial begin
-		RST_TEST = 0;
 		ADDR_TEST = 300; DATA_TEST = 300; WRITE_TEST = 1; #10;
-		RST_TEST = 1;
 		ADDR_TEST = 300; DATA_TEST = 300; WRITE_TEST = 1; #10;
 		ADDR_TEST = 300; DATA_TEST = 1; WRITE_TEST = 0; #10;
 		ADDR_TEST = 299; DATA_TEST = 1; WRITE_TEST = 1; #10;
--- implementation/PC_INC_TEST.v
+++ implementation/PC_INC_TEST.v
@@ -1,15 +1,16 @@
+`timescale 1ns/1ps
 
-module PC_INC_TEST;
-reg [15:0] SRCA_TEST;
+module PC_INC_TEST();
+
 reg [15:0] SRCB_TEST;
-reg ALU_OP
+reg ALU_OP;
 reg [15:0] PC_IN_TEST;
 reg CLOCK;
 reg PC_RESET;
 reg PC_EN;
 wire ALU_ZERO_FLAG;
 wire [15:0] ALU_OUT_TEST;
-wire [15:0] PC_OUT_TEST;
+
 
 wire [15:0] connect;
 
@@ -21,17 +22,38 @@ wire [15:0] connect;
 //ALU uut2(SRCA_TEST, SRCB_TEST, ALU_OP, ALU_OUT_TEST, ALU_ZERO_TEST);
 
 
-ProgramCounter uut1(CLOCK, PC_RESET, connect, PC_EN, PC_OUT_TEST);
+ProgramCounter pc_test (
+        .clock(CLOCK), 
+        .reset(PC_RESET), 
+        .jump_addr(ALU_OUT_TEST), 
+        .jump_en(PC_EN), 
+        .pc(connect)
+    );
 
+ALU ALU_inst
+(
+	.SRCA(connect) ,	// input [15:0] SRCA_sig
+	.SRCB(SRCB_TEST) ,	// input [15:0] SRCB_sig
+	.ALUOP(ALU_OP) ,	// input  ALUOP_sig
+	.ALUOUT(ALU_OUT_TEST) ,	// output [15:0] ALUOUT_sig
+	.ZERO(ALU_ZERO_FLAG) 	// output  ZERO_sig
+);
 
-
-ALU uut2(SRCA_TEST, SRCB_TEST, ALU_OP, connect, ALU_ZERO_TEST);
 
 	initial begin
 		CLOCK = 0;
-		forever begin
-			#10; CLOCK = ~CLOCK;
-		end
 	end
 	
+	
+	always #10 CLOCK = ~CLOCK;
 	//finish tests here
+	
+	
+	initial begin
+		PC_RESET = 1; #10;
+		PC_RESET = 0; #10;
+		ALU_OP = 0; SRCB_TEST = 2; PC_EN = 0; #10;
+		#100;
+		$stop;
+	end
+endmodule
